cfad3fb363b5d11ef2ff82cdd4e2345f54575e00
Josh Imbriani <joshimbriani@gmail.com>
2017-12-26 19:36:31 -0600

    Added ebayWatcher

--- modules/ebayWatcher.py
+++ modules/ebayWatcher.py
@@ -1,2 +1,27 @@
-def run():
-    click.echo("This is the Ebay watcher app!")
+import datetime
+
+import click
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+
+from utilities.misc import getCredentials
+
+def run(argDict):
+    ebayAppKey = getCredentials("ebayWatcher")["appID"]
+    try:
+        api = Connection(appid=ebayAppKey, config_file=None)
+        response = api.execute('findItemsAdvanced', {'keywords': 'legos'})
+
+        assert(response.reply.ack == 'Success')
+        assert(type(response.reply.timestamp) == datetime.datetime)
+        assert(type(response.reply.searchResult.item) == list)
+
+        click.echo(response.reply.searchResult.item)
+
+        item = response.reply.searchResult.item[0]
+        assert(type(item.listingInfo.endTime) == datetime.datetime)
+        assert(type(response.dict()) == dict)
+
+    except ConnectionError as e:
+        print(e)
+        print(e.response.dict())
--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
         "Click",
         "python-crontab",
         "twilio",
-        "sendgrid"
+        "sendgrid",
+        "ebaysdk"
     ],
     entry_points='''
         [console_scripts]
