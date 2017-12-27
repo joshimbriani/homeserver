1fd456514c282e039381fec37ef047a20626dec9
Josh Imbriani <joshimbriani@gmail.com>
2017-12-26 20:49:11 -0600

    Modified ebay module

--- modules/ebayWatcher.py
+++ modules/ebayWatcher.py
@@ -1,4 +1,7 @@
 import datetime
+import time
+from datetime import datetime
+import re
 
 import click
 from ebaysdk.exception import ConnectionError
@@ -6,21 +9,29 @@ from ebaysdk.finding import Connection
 
 from utilities.misc import getCredentials
 
+
 def run(argDict):
     ebayAppKey = getCredentials("ebayWatcher")["appID"]
+    searchlist = []
+    if "watchlist" in argDict:
+        searchlist = argDict['watchlist']
+    elif "watchlist" in getCredentials("ebayWatcher"):
+        searchlist = getCredentials("ebayWatcher")["watchlist"]
+    else:
+        searchlist = [{"name": 'Decidueye GX'}]
     try:
         api = Connection(appid=ebayAppKey, config_file=None)
-        response = api.execute('findItemsAdvanced', {'keywords': 'legos'})
-
-        assert(response.reply.ack == 'Success')
-        assert(type(response.reply.timestamp) == datetime.datetime)
-        assert(type(response.reply.searchResult.item) == list)
-
-        click.echo(response.reply.searchResult.item)
-
-        item = response.reply.searchResult.item[0]
-        assert(type(item.listingInfo.endTime) == datetime.datetime)
-        assert(type(response.dict()) == dict)
+        for search in searchlist:
+            response = api.execute('findItemsAdvanced', {'keywords': search['name']})
+            for item in response.dict()['searchResult']['item']:
+                price = float(item['sellingStatus']['convertedCurrentPrice']['value'])
+                endTime = datetime.strptime(item['listingInfo']['endTime'], '%Y-%m-%dT%H:%M:%S.000Z')
+                if price < search['price'] * 0.9 and (endTime-datetime.now()).seconds < 60*60*2 and not re.search("(World Champion|China|Chinese|Japan|Coin|Jumbo)", item['title'], re.IGNORECASE):
+                    click.echo("The item {} is selling for ${} with {} minutes left. This is {}\% of the market value. The URL is {}.".format(
+                        item['title'], float(item['sellingStatus']['convertedCurrentPrice']['value']), (endTime-datetime.now()).seconds / 60, 
+                        float(item['sellingStatus']['convertedCurrentPrice']['value'])/search['price'], item['viewItemURL']))
+                    
+            time.sleep(2)
 
     except ConnectionError as e:
         print(e)
