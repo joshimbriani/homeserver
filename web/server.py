b7cad8419f7fcb6daf3315c51981740c38f49c50
Josh Imbriani <joshimbriani@gmail.com>
2019-01-22 09:12:48 -0700

    Serving static file

--- web/server.py
+++ web/server.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 from homeserver.web.routes.waitTime import waittime
 
 app = Flask(__name__)
+app._static_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets')
 app.register_blueprint(waittime, url_prefix='/api/waittime')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", defaults={'u_path': ''})
+@app.route('/<path:u_path>')
+def hello(u_path):
+    return app.send_static_file('index.html')
