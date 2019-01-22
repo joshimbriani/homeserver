674a8b0e91bf8d4c722aa67b10c81a1ce0ddb4ab
Josh Imbriani <joshimbriani@gmail.com>
2019-01-22 10:46:00 -0700

    Adding shell of React app

--- web/server.py
+++ web/server.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
-from homeserver.web.routes.waitTime import waittime
+#from homeserver.web.routes.waitTime import waittime
 
 app = Flask(__name__)
-app._static_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets')
-app.register_blueprint(waittime, url_prefix='/api/waittime')
+#app.register_blueprint(waittime, url_prefix='/api/waittime')
 
 @app.route("/", defaults={'u_path': ''})
 @app.route('/<path:u_path>')
 def hello(u_path):
-    return app.send_static_file('index.html')
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
