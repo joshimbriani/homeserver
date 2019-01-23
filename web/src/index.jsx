7eeaf975e0a8d4633888902ee59b071858c81839
Josh Imbriani <joshimbriani@gmail.com>
2019-01-22 19:33:13 -0700

    Starting to build web interface

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import AppRouter from './approuter.jsx'; 
 
 class App extends React.Component {
     render() {
         return (
-            <div>My Flask React App!</div>
+            <AppRouter />
         );
     }
 }
